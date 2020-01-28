4b43a9b5d2270f53302192f07c3237b563194599
Mykola Slepko <slepko13@gmail.com>
2020-01-28 16:02:47 +0200

    Updates

---
+++ precache-manifest.b272031cb03702128f9cb86d9ac08e2f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b34d5e91408886c1cbb96b0b7526699",
+    "url": "/reseau/index.html"
+  },
+  {
+    "revision": "a23c4031a38b0f38e1f3",
+    "url": "/reseau/static/css/main.56617cc2.chunk.css"
+  },
+  {
+    "revision": "b97dc0715dbbc8ab020c",
+    "url": "/reseau/static/js/2.ff1ed8c2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/reseau/static/js/2.ff1ed8c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a23c4031a38b0f38e1f3",
+    "url": "/reseau/static/js/main.6f978576.chunk.js"
+  },
+  {
+    "revision": "66053c488963020ed3eb",
+    "url": "/reseau/static/js/runtime-main.4374a869.js"
+  },
+  {
+    "revision": "305f5d805ea5b6e3ac478cb1ae906dd6",
+    "url": "/reseau/static/media/Spinner-1s-550px transparent.305f5d80.svg"
+  },
+  {
+    "revision": "f07d66e00150e23a0d8b2e10b7b0f86b",
+    "url": "/reseau/static/media/alt.f07d66e0.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/reseau/static/media/logo.ee7cd8ed.svg"
+  }
+]);
